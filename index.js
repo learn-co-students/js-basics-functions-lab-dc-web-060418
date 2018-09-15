00e2f85ed1fd5fbcdfc854fe7ce29f5920dbd7ac
j-shilling <shilling.jake@gmail.com>
2018-09-15 10:21:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(curloc) {
+  max = Math.max(curloc, 42);
+  min = Math.min(curloc, 42);
+  return max - min;
+}
+
+function distanceFromHqInFeet(curloc) {
+  return distanceFromHqInBlocks(curloc) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  max = Math.max(start, end)
+  min = Math.min(start, end)
+  return (max - min) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+  feet = distanceTravelledInFeet(start, end);
+
+  if (feet < 400) {
+    return 0;
+  } else if (feet <= 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
